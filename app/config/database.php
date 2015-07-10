ac9743e05d2caa3491ec9af4a70b49b7499a3b59
Nicolas Prado <npradomendo@gmail.com>
2015-07-10 12:20:15 -0500

    Update database.php

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'clean',
-			'username'  => 'root',
-			'password'  => 'root',
+			'database'  => 'localhost',
+			'username'  => 'localhost',
+			'password'  => 'localhost',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
